a3b46dc426d4112dc7b9467f02d3585d8a293502
HybridMichal <118260010+HybridMichal@users.noreply.github.com>
2024-11-25 10:09:53 +0100

    pluh

--- quizzlers/src/App.vue
+++ quizzlers/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-    <h1 className="text-3xl font-bold underline">
-      
+    <h1 className="text-3xl font-bold justify-self-center">
+      tuto sa bude nachadzat uplne ppc stranka jedneho dna
     </h1>
 </template>
